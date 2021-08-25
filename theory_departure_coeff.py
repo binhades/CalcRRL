b7b7e665bd37c49c8054d27e07d9a157314c1202
Alien Hades <binhades@users.noreply.github.com>
2021-08-25 15:17:18 +0800

    add plot and beta_n

--- theory_departure_coeff.py
+++ theory_departure_coeff.py
@@ -7,58 +7,77 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyrrl as rrl
 
+def plot(n_arr, bn, beta_n, labels, format='png', issave=False, isshow=True):
+#==============================================================================
+    fig = plt.figure(figsize=(10,6))
+#==============================================================================
+    ax1 = plt.subplot(211)
+    for i, lab in enumerate(labels):
+        ax1.plot(n_arr,bn[i],label=lab)
+
+    ax1.set_title('Departure Coefficient')
+    ax1.set_xlabel('Principal energy level $n$')
+    ax1.set_ylabel('$b_n$')
+    ax1.legend()
+#==============================================================================
+    ax2 = plt.subplot(212)
+    for i, lab in enumerate(labels):
+        ax2.plot(n_arr,beta_n[i],label=lab)
+
+    ax2.set_title('Amplification Factor')
+    ax2.set_xlabel('Principal energy level $n$')
+    ax2.set_ylabel(r'$\beta_n$')
+    ax2.legend()
+#==============================================================================
+    plt.tight_layout()
+    if issave:
+        plt.savefig('departure_coeff.png',format=format,dpi=300,bbox_inches=tight)
+    if isshow:
+        plt.show()
+    plt.close(fig)
+    return 0
+#==============================================================================
+
 def main(args):
 
-#==============================================================================
-# Check functions from atom.py
-# --------------------------------
-#    n_u = args.N_u
-#    n_l = args.N_l
-#    f_ul = rrl.atom.oscillator_strength(n_u,n_l,method=args.method)
-#    g_ul = rrl.atom.gaunt_factor(n_u,n_l,method=args.method)
-#    A_ul0= rrl.atom.Einstein_Coefficient_A(n_u,n_l,method=args.method)
-#    A_ul1= rrl.atom.Einstein_Coefficient_A(n_u,n_l)
-#    lt = rrl.atom.energy_level_lifetime(n_u)
-#    alpha = rrl.atom.recombination_rate(args.N,args.Te,method=args.method)
-
-# -----------------------------------------------------------------------------
-# Print out results
-# -----------------------------------------------------------------------------
-#    print('N_u: {:d};\nN_l: {:d};\nMethod: {:d}'.format(n_u,n_l, args.method))
-#    print('Oscillator Strength: f = {:f}'.format(f_ul))
-#    print('Gaunt Factor:        g = {:f}'.format(g_ul))
-#    print('Einstein A:          A = {:f}'.format(A_ul0))
-#    print('Einstein A default:  A = {:f}'.format(A_ul1))
-#    print('EnergyLeve Lifetime:     {:f}'.format(lt))
-#    print('Recombination rate: {:e}'.format(alpha)) 
-#==============================================================================
     n_arr = np.arange(args.N_min,args.N_max+1)
-    bn = rrl.bn.departure_coefficients(n_min=args.N_min,n_max=args.N_max,n_cut=args.N_cut,Te=args.Te,Ne=args.Ne)
-    plt.plot(n_arr,bn)
-    plt.show()
+    Te = args.Te
+
+    if args.Ne is None:
+        Ne_list = [ 1, 10, 1e2, 1e3, 1e4, 1e5]
+    else:
+        Ne_list = [args.Ne]
+
+    labels = []
+    bn_list = []
+    beta_n_list = []
+
+    for Ne in Ne_list:
+        labels.append('Te:{:.0f}K; Ne:{:.0E}'.format(Te,Ne)+'cm$^{-3}$;')
+        bn = rrl.bn.departure_coefficients(n_min=args.N_min, n_max=args.N_max, \
+                                           n_cut=args.N_cut, Te=Te, Ne=Ne)
+        beta_n = rrl.bn.amplification_factor(bn,Te)
+        bn_list.append(bn)
+        beta_n_list.append(beta_n)
+    plot(n_arr,bn_list,beta_n_list,labels)
     return 0
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--Te',   type=float, default=10000, \
-            help='Electron Temperature in K, default 10000 K')
-    parser.add_argument('--Ne',   type=float, default=10000, \
-            help='Electron Density in cm-3, default 10000 cm-3')
+    parser.add_argument('--Te',   type=float, default=1e4, \
+            help='Electron Temperature in K')
+    parser.add_argument('--Ne',   type=float, \
+            help='Electron Density in cm-3')
     parser.add_argument('--N',    type=int,   default=40, \
             help='Primary quantum level, default 40')
-    parser.add_argument('--N_cut', type=int,   default=1000, \
-            help='cut of Primary quantum level, default 10')
-    parser.add_argument('--N_min', type=int,   default=40, \
+    parser.add_argument('--N_cut', type=int,   default=800, \
+            help='cut of Primary quantum level, default 800')
+    parser.add_argument('--N_min', type=int,   default=20, \
             help='lower limit of Primary quantum level, default 10')
-    parser.add_argument('--N_max', type=int,   default=200, \
-            help='upper limit Primary quantum level, default 507')
-    parser.add_argument('--N_l',  type=int,   default=10, \
-            help='lower level, default 10')
-    parser.add_argument('--N_u',  type=int,   default=11, \
-            help='upper level, default 11')
+    parser.add_argument('--N_max', type=int,   default=300, \
+            help='upper limit Primary quantum level, default 300')
     parser.add_argument('--case', type=int,   default=2, \
             help='1 for Case A, 2 for Case B, default 2')
-    #parser.add_argument('--method', type=int,   default=1,   help='method')
     parser.add_argument('--method', type=str,   default='S59', \
             help='method')
 
